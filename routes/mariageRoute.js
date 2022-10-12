86992d5e1422ba9435badd42e41ba28d77c36821
Herman-Motcheyo <hermanmotcheyo@gmail.com>
2022-10-12 09:46:08 +0200

    add datawarehouse

--- routes/mariageRoute.js
+++ routes/mariageRoute.js
@@ -3,7 +3,7 @@ const router = express.Router()
 //let MariageController = require("../services/mariageService");
 let {mariageInfo , getPublicationTable , getMariageTable } = require("../controllers/mariageController");
 
-router.route("/").get(mariageInfo) ; //permet de retourner la tableau des mariages
+router.route("/mariage").get(mariageInfo) ; //permet de retourner la tableau des mariages
 router.route("/publication").get(getPublicationTable);
 router.route("/mariagetable").get(getMariageTable)
 module.exports = router ;
